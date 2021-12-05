331a8cb5d0b246d7e432cddf1c7f0aa51be6a92d
narendras9 <narendra1982@gmail.com>
2021-12-05 09:40:40 +0530

    Added 3 new ML models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 ﻿print("Adding all ML related code here")
+
+
+def model1():
+    print("adding code for ML Model 1")
+
+
+def model2():
+    print("adding code for ML Model 2")
+
+
+def model3():
+    print("adding code for ML Model 3")
